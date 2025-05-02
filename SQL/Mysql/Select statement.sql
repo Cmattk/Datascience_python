4f0bdb15df9e4c3b2aeae4b3672c358ab422f796
Cmattk <mathiaskasina@gmail.com>
2025-05-02 10:01:15 +0300

    add

---
+++ SQL/Mysql/Select statement.sql
@@ -0,0 +1,7 @@
+SELECT *, age + 10
+FROM parks_and_recreation.employee_demographics 
+where age <= 40;
+
+select distinct first_name,gender from employee_demographics;
+# PEMDAS
+select * from parks_and_recreation.employee_salary;
