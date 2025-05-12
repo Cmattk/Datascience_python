1f61a1a3a102f557e562df541ead24cba02909f4
Celestine Mathias Kasina <mathiaskasina@gmail.com>
2025-05-12 13:09:34 +0300

    Update create a database.py

--- SQL/Mysql/python mysql/create a database.py
+++ SQL/Mysql/python mysql/create a database.py
@@ -2,7 +2,7 @@ from mysql.connector import connection
 
 conn = connection.MySQLConnection(host = 'localhost',
                                   user = 'root',
-                                  password = '@Cmkf2100#',
+                                  password = '',
                                   database = 'geeks4geeks')
 
 # preparing a cursor object
